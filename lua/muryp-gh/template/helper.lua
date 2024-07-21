4b477db4ccb05fdad0055238e6fa366ba3d6b311
alifprihantoro <alifprihantoro@gmail.com>
2024-07-21 13:45:28 +0700

    fix: update helper

--- lua/muryp-gh/template/helper.lua
+++ lua/muryp-gh/template/helper.lua
@@ -1,11 +1,9 @@
 return [[
 <!--
 how use it:
-- <leader>gid => DELETE
-- <leader>gie => EDIT
-- <leader>gip => POST
-- <leader>giu => UPDATE
-- enter in link to open browser (use vimwiki)
+- GhIssue <core-cmd>
+
+see README for more info
 
 NOTE : puth text under comment(all comment eddit is no effect )
 -->
